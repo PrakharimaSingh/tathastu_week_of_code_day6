b52073e33e13e2ea0eb6b7601f7fe0f321e964f0
prakharima singh <65663329+PrakharimaSingh@users.noreply.github.com>
2020-05-29 22:34:58 +0530

    Create program11.py

---
+++ program11.py
@@ -0,0 +1,6 @@
+n = int(input("Enter size of list: "))
+list = []
+for i in range(n):
+     li.append(int(input("Enter the elements : "))) 
+list.sort(reverse = True)
+print("Largest product of 3 nummbers in list is:", li[0]*li[1]*li[2])
